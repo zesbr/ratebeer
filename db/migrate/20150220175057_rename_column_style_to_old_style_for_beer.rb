70084b707b66def696ea00eba2cef845bb1cb2e8
zesbr <jesse_ruuth@hotmail.com>
2015-02-20 23:29:08 +0200

    Viikko 5

--- db/migrate/20150220175057_rename_column_style_to_old_style_for_beer.rb
+++ db/migrate/20150220175057_rename_column_style_to_old_style_for_beer.rb
@@ -1,6 +1,6 @@
 class RenameColumnStyleToOldStyleForBeer < ActiveRecord::Migration
   def change
   	rename_column :beers, :style, :old_style
-  	add_column :beers, :style_id, :integer
+  	
   end
 end
